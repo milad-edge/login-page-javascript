ee58d211183d71edeb486e19befd5b85ae9d2bb7
milad hamedi nasab <milad.hamedinasab@gmail.com>
2018-12-11 10:15:49 -0500

    Final Project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import { person } from './data';
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+// Get id from DOM
+const lgUsername = document.querySelector('#lg-username');
+const lgPassword = document.querySelector('#lg-password');
+const lgbtn = document.querySelector('#lg-btn');
+const lgAlert = document.querySelector('#lg-alert');
 
-getData('https://jsonplaceholder.typicode.com/posts');
+// Evnet listener for login button
+document.querySelector('#lg-btn').addEventListener('click', authLogin);
+
+// function for click event
+function authLogin(e) {
+    if(lgUsername.value === '' || lgPassword.value === '') {
+
+        Alert('Please fill in the fields!', 'alert-danger');
+
+    } else if (lgUsername.value !== person.username && lgPassword.value !== person.password) {
+
+        Alert('Your username or  your password is incorrect!', 'alert-danger');
+
+    }  else {
+        Alert('congratulations ! You are logged in.', 'alert-success');
+    }
+
+    e.preventDefault();
+}
+
+// alert function
+function Alert(message, type) {
+    lgAlert.innerHTML = `
+        <div class="alert alert-dismissible ${type}">
+            <span>${message}</span>
+        </div>
+    `
+}
